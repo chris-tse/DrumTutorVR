2f0564899a95b1268afb697983f12b1740b3a03c
Jacob Young <jacobyoung@ou.edu>
2018-03-04 10:13:04 -0600

    Altered stage and shrunk objs

--- scripts/init.js
+++ scripts/init.js
@@ -1,10 +1,19 @@
 const transparentBlue = new THREE.MeshBasicMaterial({color: 0x6262E3, opacity: 0.5});
 
 const toMakeBlue = [...document.querySelectorAll(".makeBlue")];
+const objToScale = [...document.querySelectorAll("a-entity")];
 
 toMakeBlue.forEach(drum => {
     drum.setAttribute('material', {
         color: '#6262E3',
         opacity: 0.5
+    })
+})
+
+objToScale.forEach(obj => {
+    obj.setAttribute('scale', {
+        x: 0.5,
+        y: 0.5,
+        z: 0.5
     })
 })
